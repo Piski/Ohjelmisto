3d192215303a71577d5e64ee64a931f4cc0b78bc
Sergei Liski <sergei.liski@maanmittauslaitos.fi>
2015-10-02 23:05:31 +0300

    Add MockEsimerkki

---
+++ MockEsimerkki/MockEsimerkki/Hinnoittelija.java
@@ -0,0 +1,8 @@
+package mockesimerkki;
+
+public class Hinnoittelija {
+
+    public float getAlennusProsentti(Asiakas asiakas, Tuote tuote) {
+        return 0.0f;
+    }
+}
---
+++ MockEsimerkki/MockEsimerkki/TilaustenKäsittely.java
@@ -0,0 +1,21 @@
+
+package mockesimerkki;
+
+public class TilaustenKäsittely {
+
+    private Hinnoittelija hinnoittelija;
+
+    public void setHinnoittelija(Hinnoittelija hinnoittelija) {
+        this.hinnoittelija = hinnoittelija;
+    }
+
+    public void käsittele(Tilaus tilaus) {
+
+        float alennusProsentti = hinnoittelija.getAlennusProsentti(
+                tilaus.getAsiakas(), tilaus.getTuote());
+        float alennusHinta = tilaus.getTuote().getHinta()
+                * (1 - (alennusProsentti / 100));
+        tilaus.getAsiakas().setSaldo(
+                tilaus.getAsiakas().getSaldo() - alennusHinta);
+    }
+}
